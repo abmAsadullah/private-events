dd5aa3a7997bb678e805247e4f082a98f39dc5c6
A B M Asadullah <abmasadullah@gmail.com>
2021-01-16 15:04:19 +0600

    Create normal date-time and add index and show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:title, :description)
+    params.require(:event).permit(:title, :description, :date)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+    validates :title, presence: true
+    validates :date, presence: true
 end
