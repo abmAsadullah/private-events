c81a628ef4bb4a1c710184bed7544e9259daa376
A B M Asadullah <abmasadullah@gmail.com>
2021-01-16 18:58:33 +0600

    Display event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+    has_many :event_attendees,  foreign_key: :attended_event_id
+    has_many :attendees, 		through: :event_attendees
+    
     validates :title, presence: true
     validates :date, presence: true
 end
