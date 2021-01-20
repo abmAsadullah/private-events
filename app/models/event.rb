79485b5b81ce64479b4b30263bb59cbc7decc34f
A B M Asadullah <abmasadullah@gmail.com>
2021-01-21 01:17:14 +0600

    Linters fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User', foreign_key: :creator_id
-    has_many :event_attendees,  foreign_key: :attended_event_id
-    has_many :attendees, 		through: :event_attendees
-    
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :date, presence: true
-    scope :upcoming, -> { where('date >= ?', DateTime.now) }
-    scope :past, -> { where('date < ? ', DateTime.now) }
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
+  scope :upcoming, -> { where('date >= ?', DateTime.now) }
+  scope :past, -> { where('date < ? ', DateTime.now) }
 end
