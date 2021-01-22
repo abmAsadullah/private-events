3b3d2d83ef34e40111e4be55aaecaa39a7b87461
A B M Asadullah <abmasadullah@gmail.com>
2021-01-22 22:50:47 +0600

    Linter fix

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-    describe 'association' do
-      it { should belong_to(:creator).class_name('User').with_foreign_key(:creator_id) }
-      it { should have_many(:event_attendees).with_foreign_key(:attended_event_id) }
-      it { should have_many(:attendees).through(:event_attendees) }
-    end
+  describe 'association' do
+    it { should belong_to(:creator).class_name('User').with_foreign_key(:creator_id) }
+    it { should have_many(:event_attendees).with_foreign_key(:attended_event_id) }
+    it { should have_many(:attendees).through(:event_attendees) }
+  end
 
-    describe 'validations' do
-      it { should validate_presence_of(:title) }
-      it { should validate_presence_of(:description) }
-      it { should validate_presence_of(:date) }
-    end
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:date) }
+  end
 end
