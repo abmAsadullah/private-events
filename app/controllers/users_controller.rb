6e36b70d5e485cabc9aa6eb46d89ecc78bd61047
A B M Asadullah <abmasadullah@gmail.com>
2021-01-23 23:55:09 +0600

    RSPEC Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @created_events = @user.events
+    current_user = @user
+    @created_events = current_user.events
     @upcoming_events = @user.attended_events.upcoming
     @prev_events = @user.attended_events.past
   end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,15 +1,74 @@
+# rubocop:disable Layout/LineLength
+
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  describe 'association' do
-    it { should belong_to(:creator).class_name('User').with_foreign_key(:creator_id) }
-    it { should have_many(:event_attendees).with_foreign_key(:attended_event_id) }
-    it { should have_many(:attendees).through(:event_attendees) }
+  describe 'title presence' do
+    it 'if title not present' do
+      u = Event.new
+      u.title = ''
+      u.valid?
+      expect(u.errors[:title]).to include("can't be blank")
+    end
+
+    it 'if title present' do
+      u = Event.new
+      u.title = 'user'
+      u.valid?
+      expect(u.errors[:title]).to_not include("can't be blank")
+    end
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:date) }
+  describe 'description presence' do
+    it 'if description not present' do
+      u = Event.new
+      u.description = ''
+      u.valid?
+      expect(u.errors[:description]).to include("can't be blank")
+    end
+
+    it 'if description present' do
+      u = Event.new
+      u.description = 'user'
+      u.valid?
+      expect(u.errors[:description]).to_not include("can't be blank")
+    end
+  end
+
+  describe 'date presence' do
+    it 'if date not present' do
+      u = Event.new
+      u.date = ''
+      u.valid?
+      expect(u.errors[:date]).to include("can't be blank")
+    end
+
+    it 'if date present' do
+      u = Event.new
+      u.date = '2020-11-22
+      18:42:00'
+      u.valid?
+      expect(u.errors[:date]).to_not include("can't be blank")
+    end
+
+    it 'if date present but wrong format' do
+      u = Event.new
+      u.date = 'asdf'
+      u.valid?
+      expect(u.errors[:date]).to include("can't be blank")
+    end
+  end
+
+  describe 'all presence' do
+    it 'if creator_id not present' do
+      expect { Event.create!(title: 'title', description: 'description', date: '2020-11-22 18:42:00') }.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Creator must exist')
+    end
+
+    it 'if all present' do
+      User.create!(email: 'test@gmail.com', password: 'passwordtest', password: 'passwordtest')
+      expect(Event.create!(title: 'title', description: 'description', date: '2020-11-22 18:42:00', creator_id: User.find(1).id).valid?).to be true
+    end
   end
 end
+
+# rubocop:enable Layout/LineLength
